96073a62021f9778e80dfe7df62844d2e067bf59
son1228 <sonvipkl04@gmail.com>
2024-08-05 15:09:59 +0700

    [GET] /search/:suggest

--- controllers/client/search.controller.ts
+++ controllers/client/search.controller.ts
@@ -2,8 +2,10 @@ import { Request, Response } from "express";
 import Song from "../../models/song.model";
 import Singer from "../../models/singer.model";
 import { convertToSlug } from "../../helpers/convertToSlug.helper";
-// [GET] /search/result
+// [GET] /search/:type
 export const result = async (req: Request, res: Response) => {
+  const type = req.params.type;
+
   const keyword = req.query.keyword;
   // if(typeof keyword === 'string'){
   let newSongs: any = [];
@@ -19,14 +21,48 @@ export const result = async (req: Request, res: Response) => {
       const infoSinger = await Singer.findOne({
         _id: item.singerId,
       });
-      item["infoSinger"] = infoSinger;
+      if (typeof infoSinger === "object" && infoSinger != null)
+        newSongs.push({
+          id: item.id,
+          avatar: item.avatar,
+          title: item.title,
+          like: item.like,
+          slug: item.slug,
+          infoSinger: {
+            fullName: infoSinger.fullName,
+          },
+        });
     }
-    console.log(songs);
-    newSongs = songs;
   }
-  console.log(newSongs);
-  res.render("client/pages/search/result.pug", {
-    keyword,
-    songs: newSongs,
+  switch (type) {
+    case "result":
+      res.render("client/pages/search/result.pug", {
+        keyword,
+        songs: newSongs,
+      });
+      break;
+    case "suggest":
+      try {
+        res.json({
+          code: 200,
+          message: "success",
+          songs: newSongs,
+        });
+      } catch (error) {
+        res.json({
+          code: 400,
+          message: error,
+        });
+      }
+    default:
+      break;
+  }
+};
+
+// [GET] /search/suggest
+export const suggest = async (req: Request, res: Response) => {
+  res.json({
+    code: 200,
+    message: "Success",
   });
 };
--- routes/client/search.route.ts
+++ routes/client/search.route.ts
@@ -2,5 +2,5 @@ import { Router } from "express";
 const router: Router = Router();
 import * as controller from "../../controllers/client/search.controller";
 
-router.get("/result", controller.result);
+router.get("/:type", controller.result);
 export const searchRoutes: Router = router;
